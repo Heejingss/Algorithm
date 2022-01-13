3661c6e78fb3713a7e4f2628ad3e890ac2153f22
Heejingss <yhj980810@hanmail.net>
2022-01-13 15:06:52 +0900

    20220113_1546_2708

---
+++ src/main/java/heejin/boj/Boj_1546.java
@@ -0,0 +1,30 @@
+package heejin.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 선언
+        String str = bf.readLine();
+        int count = Integer.parseInt(str);
+        String score = bf.readLine();
+        String arr[] = score.split(" ");
+        int max = 0;
+        float sum = 0;
+
+        for (int i = 0; i < count; i++) {
+            if (max < Integer.parseInt(arr[i]))
+                max = Integer.parseInt(arr[i]);
+        }
+
+        for (int i = 0; i < count; i++) {
+
+            sum = sum + (Float.valueOf(arr[i]) / max * 100);
+        }
+
+        System.out.println(sum / count);
+
+    }
+}
---
+++ src/main/java/heejin/boj/Test.java
@@ -0,0 +1,21 @@
+package heejin.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 선언
+        String str = bf.readLine();
+        String arr[] = str.split(" ");
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += Integer.parseInt(arr[i]);
+        }
+
+        System.out.println(sum);
+
+    }
+}
