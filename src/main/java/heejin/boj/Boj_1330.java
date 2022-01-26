2684ef4f782209ead6354df7b1da551ae32c204c
Heejingss <yhj980810@hanmail.net>
2022-01-26 21:38:11 +0900

    Boj_1330 //하기싫은날

---
+++ src/main/java/heejin/boj/Boj_1330.java
@@ -0,0 +1,22 @@
+package heejin.boj;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_1330 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        if (num1 > num2) {
+            System.out.println(">");
+        } else if (num1 < num2) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+
+    }
+}
