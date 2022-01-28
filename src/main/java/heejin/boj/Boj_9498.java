d9e73dc4a65d9baa7a3e7ed239a2e532369042ea
Heejingss <yhj980810@hanmail.net>
2022-01-28 16:54:05 +0900

    fix: Boj_9498

--- src/main/java/heejin/boj/Boj_9498.java
+++ src/main/java/heejin/boj/Boj_9498.java
@@ -1,20 +1,21 @@
+package heejin.boj;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Boj_9498 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int score = Integer.parseInt(st.nextToken());
-      
 
-        if (score >=90) {
+        if (score >= 90) {
             System.out.println("A");
-        } else if (score>=80) {
+        } else if (score >= 80) {
             System.out.println("B");
-        }  else if (score>=70) {
+        } else if (score >= 70) {
             System.out.println("C");
-        }   else if (score>=60) {
+        } else if (score >= 60) {
             System.out.println("D");
         } else {
             System.out.println("F");
