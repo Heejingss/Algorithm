0e54da9811ff43ef3ce6f2a4f419fcf27af7642c
Heejingss <yhj980810@hanmail.net>
2022-01-30 23:01:52 +0900

    Boj_3040 //0130

---
+++ src/main/java/heejin/boj/Boj_3040.java
@@ -0,0 +1,50 @@
+package heejin.boj;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_3040 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> numList = new ArrayList<>();
+        int sum = 0;
+        int overNum = 0;
+
+        for (int i = 0; i < 9; i++) {
+
+            numList.add(Integer.parseInt(br.readLine()));
+            sum = sum + numList.get(i);
+
+        }
+
+        overNum = sum - 100;
+
+        for (int i = 0; i < 9; i++) {
+
+            if ((numList.contains(overNum - numList.get(i))) && (numList.get(i) != overNum / 2)) {
+
+                int indexNum = numList.indexOf(overNum - numList.get(i));
+                if (i < indexNum) {
+                    numList.remove(i);
+                    numList.remove(indexNum - 1);
+                } else {
+                    numList.remove(indexNum);
+                    numList.remove(i - 1);
+
+                }
+
+                break;
+
+            }
+        }
+
+        for (int i = 0; i < 7; i++) {
+
+            System.out.println(numList.get(i));
+
+        }
+
+    }
+
+}
