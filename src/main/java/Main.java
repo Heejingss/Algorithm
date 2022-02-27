f014f6995d1e009bc5f2df68217564dd82a78466
Heejingss <yhj980810@hanmail.net>
2022-02-27 20:03:30 +0900

    //0227

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,37 @@
 import java.io.BufferedReader;
-//import java.io.BufferedWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-//import java.io.OutputStreamWriter;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Class 뱃지
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-
-        for (int i = 1; i <= 9; i++) {
-            System.out.println(n + " * " + i + " = " + n * i);
+        st = new StringTokenizer(br.readLine());
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
         }
+        int min = list[0];
+        int max = list[0];
+
+        for (int i = 1; i < n; i++) {
+            if (list[i] > max) {
+                max = list[i];
+            } else if (list[i] < min) {
+                min = list[i];
+            }
+        }
+        bw.append(min + " ");
+        bw.append(max + "");
+
+        bw.flush();
+        bw.close();
 
     }
 
