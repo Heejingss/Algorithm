34279b2a6db5ea8f660510324457210904d97bef
Heejingss <yhj980810@hanmail.net>
2022-02-28 19:10:29 +0900

    Boj_8958

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,33 +6,43 @@ import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, NumberFormatException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int[] list = new int[n];
+
+        int n = Integer.parseInt(br.readLine());
+        String[] list = new String[n];
+        int point = 0;
         for (int i = 0; i < n; i++) {
-            list[i] = Integer.parseInt(st.nextToken());
+            list[i] = br.readLine();
         }
-        int min = list[0];
-        int max = list[0];
 
-        for (int i = 1; i < n; i++) {
-            if (list[i] > max) {
-                max = list[i];
-            } else if (list[i] < min) {
-                min = list[i];
+        for (int i = 0; i < n; i++) {
+            int tempPoint = 0;
+            if (list[i].charAt(0) == 'O') {
+                tempPoint = 1;
+                point = 1;
             }
+            for (int j = 1; j < list[i].length(); j++) {
+                if (list[i].charAt(j) == 'O') {
+                    if (list[i].charAt(j - 1) == 'O') {
+                        tempPoint++;
+                    } else {
+                        tempPoint = 1;
+                    }
+                } else {
+                    tempPoint = 0;
+                }
+                point = point + tempPoint;
+            }
+            bw.append(point + "\n");
+            point = 0;
+            tempPoint = 0;
         }
-        bw.append(min + " ");
-        bw.append(max + "");
-
         bw.flush();
         bw.close();
-
     }
 
 }
