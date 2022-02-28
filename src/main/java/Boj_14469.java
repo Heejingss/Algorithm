5fea62b728d49691eecc4f7e54722f171f46a5a2
Heejingss <yhj980810@hanmail.net>
2022-02-28 17:20:49 +0900

    Boj_14469 arrays.sort comparator //0226

--- src/main/java/Boj_14469.java
+++ src/main/java/Boj_14469.java
@@ -19,6 +19,23 @@ public class Boj_14469 {
             list[i][1] = Integer.parseInt(st.nextToken());
         }
 
+        Arrays.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] s1, int[] s2) {
+                return s1[0] - s2[0];
+            }
+        });
+        int total = list[0][0] + list[0][1];
+
+        for (int i = 1; i < n; i++) {
+            if (total > list[i][0]) {
+                total = total + list[i][1];
+            } else {
+                total = list[i][0] + list[i][1];
+            }
+        }
+        System.out.println(total);
+
     }
 
 }
