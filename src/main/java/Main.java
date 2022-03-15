8fa5a6c5d02a09ea225c8d92983f5a37453088c7
Heejingss <yhj980810@hanmail.net>
2022-03-15 13:58:00 +0900

    Boj_1978 소수 찾기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,18 +12,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-
-        for (int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int sum = a + b;
-            bw.append("Case #" + i + ": " + sum + "\n");
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (checkDemical(Integer.parseInt(st.nextToken()))) {
+                count++;
+            }
         }
-
+        bw.append(count + " ");
         bw.flush();
         bw.close();
+    }
+
+    public static boolean checkDemical(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
