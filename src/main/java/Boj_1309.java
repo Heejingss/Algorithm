4b2d92f288213f6b1586ecf53fbededb97651d74
Heejingss <yhj980810@hanmail.net>
2022-04-04 23:25:03 +0900

    Boj_1309 DP //0404

---
+++ src/main/java/Boj_1309.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Boj_1309 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[n + 1][3];
+        dp[1][0] = dp[1][1] = dp[1][2] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+        }
+
+        long result = (dp[n][0] + dp[n][1] + dp[n][2]) % 9901;
+        bw.write(result + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
