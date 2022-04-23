12fc341428d05e649cf301cbde0b2b8bba432350
Heejingss <yhj980810@hanmail.net>
2022-04-23 15:51:08 +0900

    Boj_2979 //0423

--- src/main/java/Boj_2979.java
+++ src/main/java/Boj_2979.java
@@ -10,27 +10,31 @@ public class Boj_2979 {
         int fee1 = Integer.parseInt(st.nextToken());
         int fee2 = Integer.parseInt(st.nextToken());
         int fee3 = Integer.parseInt(st.nextToken());
-
         int totalFee = 0;
+        int[][] list = new int[3][101];
 
-        StringTokenizer stA = new StringTokenizer(br.readLine());
-        int truck1_arrival = Integer.parseInt(stA.nextToken());
-        int truck1_leave = Integer.parseInt(stA.nextToken());
-        StringTokenizer stB = new StringTokenizer(br.readLine());
-        int truck2_arrival = Integer.parseInt(stB.nextToken());
-        int truck2_leave = Integer.parseInt(stB.nextToken());
-        StringTokenizer stC = new StringTokenizer(br.readLine());
-        int truck3_arrival = Integer.parseInt(stC.nextToken());
-        int truck3_leave = Integer.parseInt(stC.nextToken());
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
 
-        if ((truck1_leave > truck2_arrival) && (truck1_leave > truck3_arrival)) {
-            if (truck1_leave > truck2_leave) {
-                if (truck2_leave > 0)
-                    totalFee = (truck2_leave - truck2_arrival) * fee2;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for (int j = a; j < b; j++) {
+                list[i][j] = 1;
             }
         }
 
-        // int totalTime=
+        for (int i = 0; i < 101; i++) {
+            if (list[0][i] + list[1][i] + list[2][i] == 3) {
+                totalFee = totalFee + 3 * fee3;
+            } else if (list[0][i] + list[1][i] + list[2][i] == 2) {
+                totalFee = totalFee + 2 * fee2;
+            } else if (list[0][i] + list[1][i] + list[2][i] == 1) {
+                totalFee = totalFee + 1 * fee1;
+            }
+        }
+        System.out.println(totalFee);
+
     }
 
 }
