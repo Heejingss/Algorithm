bb770ff16d2a042676a7ccd6c3c9c9ff2442c199
Heejingss <yhj980810@hanmail.net>
2022-05-02 12:49:56 +0900

    Boj_11399 Greedy

--- src/main/java/Boj_11399.java
+++ src/main/java/Boj_11399.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Boj_11399 {
@@ -22,9 +20,7 @@ public class Boj_11399 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 tempsum = tempsum + list[j];
-
             }
-            System.out.println(tempsum);
             sum += tempsum;
             tempsum = 0;
         }
