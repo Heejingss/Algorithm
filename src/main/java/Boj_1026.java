cccc8eb684de650e8e5a41d5759765e577ddc235
Heejingss <yhj980810@hanmail.net>
2022-05-02 12:48:52 +0900

    Boj_1026 Greedy

--- src/main/java/Boj_1026.java
+++ src/main/java/Boj_1026.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Boj_1026 {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
@@ -28,10 +29,6 @@ public class Boj_1026 {
 
         }
         System.out.print(sum);
-        // for (int i = 0; i < n; i++) {
-
-        // System.out.print(list2.get(i));
-        // }
 
     }
 
