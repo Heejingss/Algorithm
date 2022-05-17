87feed94d9a46e81b008a23895e5a67dc8dd27c4
Heejingss <yhj980810@hanmail.net>
2022-05-17 13:00:01 +0900

    Boj_7568 //0517

--- src/main/java/Boj_7568.java
+++ src/main/java/Boj_7568.java
@@ -1,26 +1,34 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Boj_7568 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[][] list = new int[n][3];
 
-        int count = Integer.parseInt(br.readLine());
-
-        ArrayList<int[]> bodyList = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int weight = Integer.parseInt(st.nextToken());
-            int height = Integer.parseInt(st.nextToken());
-            int temparr[] = { weight, height };
-            bodyList.add(temparr);
+            list[i][0] = Integer.parseInt(st.nextToken());
+            list[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < count; i++) {
-            System.out.print(bodyList.get(i)[0]);
-            System.out.println(bodyList.get(i)[1]);
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (list[i][0] < list[j][0] && list[i][1] < list[j][1]) {
+                    count++;
+                }
+            }
+            list[i][2] = count + 1;
         }
+
+        for (int i = 0; i < n; i++) {
+            bw.append(list[i][2] + " ");
+        }
+        bw.flush();
+        bw.close();
 
     }
 
