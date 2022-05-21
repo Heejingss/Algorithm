7b309d0bc9549b9249f789d05b6c40a2c862bd55
Heejingss <yhj980810@hanmail.net>
2022-05-21 17:55:28 +0900

    Boj_1541 //0521

---
+++ src/main/java/Boj_1541.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Boj_1541 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+
+        String[] list = str.split("-");
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].contains("+")) {
+                int sum = 0;
+                String[] temp = list[i].split("\\+");
+                for (int j = 0; j < temp.length; j++) {
+                    sum = sum + Integer.parseInt(temp[j]);
+                }
+                array.add(sum);
+            } else {
+                array.add(Integer.parseInt(list[i]));
+            }
+        }
+        if (array.size() == 1) {
+            bw.append(array.get(0) + "");
+        } else {
+            int base = array.get(0);
+            for (int i = 1; i < array.size(); i++) {
+                base = base - array.get(i);
+            }
+            bw.append(base + "");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+}
