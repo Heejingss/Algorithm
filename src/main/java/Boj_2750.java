debd4526101077487303bb09eceedbbb76ef7a3a
Heejingss <yhj980810@hanmail.net>
2022-06-12 12:07:03 +0900

    Boj_2750 //0612

---
+++ src/main/java/Boj_2750.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Boj_2750 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++) {
+            list[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(list);
+
+        for (int i = 0; i < n; i++) {
+            bw.append(list[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+
+}
