a45404b40262e22be9bee3e7e59dfb6b9e03b318
Heejingss <yhj980810@hanmail.net>
2022-06-17 18:48:56 +0900

    //0617

---
+++ src/main/java/Boj_10926.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Boj_10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        bw.append(str + "??!");
+        bw.flush();
+        bw.close();
+
+    }
+
+}
